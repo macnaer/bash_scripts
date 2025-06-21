8bb51c1b07cbfa5be2d6b9da60571f263d2621d2
Andrii <test@email.com>
2025-06-21 10:16:32 +0000

    Update script

--- date.sh
+++ date.sh
@@ -2,5 +2,13 @@
 
 
 #clear
-path=/home/master/bash_scripts
-date >> $path/demo.txt
+path=/home/master/bash_scripts/demo.txt
+log_path=/var/log/scripts
+
+user=$(whoami)
+current_time=$(date)
+
+echo $current_time >> $path
+
+echo $user  $current_time  >> $log_path
+
